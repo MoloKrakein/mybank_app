da1b63d42a7a4c713d31d0aae464f4e61bb55ad2
MoloKrakein <1123atha@gmail.com>
2022-08-16 11:32:53 +0700

    Create

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:firebase_core/firebase_core.dart';
 
 Future main() async {
@@ -36,15 +38,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -52,68 +45,75 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  final controller = TextEditingController();
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: TextField(controller: controller),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  final name = controller.text;
+                  createUser(name: name);
+                },
+                icon: Icon(Icons.add))
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+}
+
+class userPage extends StatefulWidget {
+  const userPage({Key? key}) : super(key: key);
+
+  @override
+  State<userPage> createState() => _userPageState();
+}
+
+class _userPageState extends State<userPage> {
+  @override
+  Widget build(BuildContext context) =>
+    Scaffold(appBar: AppBar(title: Text("Tambah Pengeluaran"),),
+    body: ListView(
+      padding: EdgeInsets.all(25),
+      children: [],
+    ),
     );
-  }
+  
+}
+
+Future createUser({required String name}) async {
+  int idgenerated = RNG();
+  final docUser = FirebaseFirestore.instance.collection('users').doc(idgenerated.toString());
+
+  final out = output(
+    id: idgenerated.toString(),
+    name: name,
+    outval: 80,
+  );
+  final json = out.toJson();
+  await docUser.set(json);
+}
+
+int RNG() {
+  Random random = new Random();
+  int rng = random.nextInt(10000);
+  return rng;
+}
+
+class output {
+  String id;
+  final String name;
+  final int outval;
+
+  output({
+    this.id = '',
+    required this.name,
+    required this.outval,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        "outval": outval,
+      };
 }
