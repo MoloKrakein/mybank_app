f03ee5685b0c77c28d7eba54f53559c4adb7eb25
MoloKrakein <1123atha@gmail.com>
2022-08-22 12:06:17 +0700

    Pick User Data "Error Json parsing"

--- lib/userdata.dart
+++ lib/userdata.dart
@@ -13,13 +13,40 @@ class UserData extends StatefulWidget {
 
 class _UserDataState extends State<UserData> {
   @override
-  Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: Text("Data Pengeluaran"),),);
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text("Data Pengeluaran"),
+        ),
+        body: StreamBuilder<List<User>>(stream: readUser(), builder: (context, snapshot) {
+          if (snapshot.hasError){
+            return Text('Woops somthing break this thing!\nError : '+snapshot.error.toString());
+          }else if (snapshot.hasData){
+            final users = snapshot.data!;
+            return ListView(
+              children: users.map(listBuilder).toList()
+            );
+          } else{
+            return Row(
+              children: [
+                Center(child: Text("Loading...."),),
+                CircularProgressIndicator()
+              ],
+            );
+          }
+        },),
+      );
   Stream<List<User>> readUser() => FirebaseFirestore.instance
       .collection('users')
       .snapshots()
-      .map((snapshot) => snapshot.docs.map((doc) => User.fromJson(doc.data())).toList());
+      .map((snapshot) =>
+          snapshot.docs.map((doc) => User.fromJson(doc.data())).toList());
 }
 
+Widget listBuilder(User user) => ListTile(
+  leading: CircleAvatar(child: Icon(getIconFromTxt('${user.kategori}'))),
+  title: Text(user.outval.toString()),
+  subtitle: Text(user.name)
+);
 // Map<String, dynamic> toJson() => {
 //         'id': id,
 //         'name': name,
@@ -28,12 +55,34 @@ class _UserDataState extends State<UserData> {
 //         "tanggal":tanggal,
 //         "jam":jam,
 //       };
+
+IconData getIconFromTxt(String iconName){
+  switch(iconName){
+    case 'Food n Beverage':{
+      return Icons.restaurant;
+    }
+    break;
+    case 'Hutang':{
+      return Icons.balance;
+    }
+    break;
+    case 'Barang Pribadi':{
+      return Icons.backpack;
+    }break;
+    default:{
+      return Icons.add_box;
+    }
+    
+
+  }
+}
+
 class User {
   String id;
   final String name;
   final int outval;
   final String kategori;
-  final DateTime tanggal;
+  final String tanggal;
   final String jam;
 
   User({
@@ -45,13 +94,12 @@ class User {
     required this.jam,
   });
 
-
-static User fromJson(Map<String, dynamic> json) => User(
-      id: json['id'],
-      name: json['name'],
-      kategori: json['kategori'],
-      outval: json['harga'],
-      tanggal: json['tanggal'],
-      jam: json['jam'],
-    );
+  static User fromJson(Map<String, dynamic> json) => User(
+        id: json['id'],
+        name: json['name'],
+        kategori: json['kategori'],
+        outval: json['harga'],
+        tanggal: json['tanggal'],
+        jam: json['jam'],
+      );
 }
